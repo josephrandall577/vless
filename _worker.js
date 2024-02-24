8f8d300038fd6dbc23f01f76b6434dbde4f4953c
josephrandall577 <116338342+josephrandall577@users.noreply.github.com>
2024-02-24 23:14:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
+let userID = '82b040b9-cef3-45d5-b9f3-ce8b438b45bb';
 
 const proxyIPs = "acjp2.cloudflarest.link";//['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
